ddabf90fed1e80ea27723bf6369362ef36710195
attaeht <attaeht@gmail.com>
2022-08-12 20:22:50 +0100

    uv about:blank

--- ehtsite/assets/games/uv/assets/proxy.js
+++ ehtsite/assets/games/uv/assets/proxy.js
@@ -1,5 +1,19 @@
 function proxy() {
-    const name = document.getElementById('jamal')
+    const name = document.getElementById('jamal');
+    name2 = name.value;
     let url = "https://ultraviolet-node.attaeht.repl.co/attaeht.html#";
-    window.location.href = url + name;
+    turl = url + name2;
+
+    win = window.open();
+        win.document.body.style.margin = '0';
+        win.document.body.style.height = '100vh';
+        var iframe = win.document.createElement('iframe');
+        iframe.style.border = 'none';
+        iframe.style.width = '100%';
+        iframe.style.height = '100%';
+        iframe.style.margin = '0';
+        iframe.src = turl;
+        win.document.body.appendChild(iframe)
+
+    window.open(win);
 }
